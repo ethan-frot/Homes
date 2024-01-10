03b111d9c4115d42c801d226b25d0c3fcd9e5ea6
Ethan <github.xdgcd@passmail.net>
2024-01-10 11:29:56 +0100

    Bug fix

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -4,7 +4,7 @@ import { HousingLocation } from '../housinglocation';
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [Input, CommonModule],
+  imports: [],
   template: ` <p>housing-location works!</p> `,
   styleUrl: './housing-location.component.css',
 })
