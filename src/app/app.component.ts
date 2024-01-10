53b7897272530fa46f5dfa2e538da4385d357402
Ethan <github.xdgcd@passmail.net>
2024-01-10 11:19:34 +0100

    Created the first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { RouterOutlet } from '@angular/router';
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  template: `<h1>Hello world!</h1>`,
 })
 export class AppComponent {
   title = 'Homes';
