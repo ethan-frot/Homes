1dd06e322e28155bcc28722705caad1acd76414f
Ethan <github.xdgcd@passmail.net>
2024-01-10 16:25:51 +0100

    Revert "Add HTTP communication (It didn't works)"
    This reverts commit 674f30d3888e8e8bb299a90f55b99d6c968150da.

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -61,11 +61,8 @@ export class DetailsComponent {
 
   constructor() {
     const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
-    this.housingService
-      .getHousingLocationById(housingLocationId)
-      .then((housingLocation) => {
-        this.housingLocation = housingLocation;
-      });
+    this.housingLocation =
+      this.housingService.getHousingLocationById(housingLocationId);
   }
   submitApplication() {
     this.housingService.submitApplication(
