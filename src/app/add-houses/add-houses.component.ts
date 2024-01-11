7b512450021ea8c6f47b2e8823d55aa7cd31aa57
Ethan <github.xdgcd@passmail.net>
2024-01-11 10:35:30 +0100

    Creating the component

---
+++ src/app/add-houses/add-houses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-houses',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-houses.component.html',
+  styleUrl: './add-houses.component.css'
+})
+export class AddHousesComponent {
+
+}
