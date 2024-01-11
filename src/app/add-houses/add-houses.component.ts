f765b022e7584812a2e9313742f05d5658ee9ef3
Ethan <github.xdgcd@passmail.net>
2024-01-11 15:18:30 +0100

    Finished the Form

--- src/app/add-houses/add-houses.component.ts
+++ src/app/add-houses/add-houses.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DisplayFormComponent } from '../display-form/display-form.component';
 
 @Component({
   selector: 'app-add-houses',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DisplayFormComponent],
   templateUrl: './add-houses.component.html',
   styleUrl: './add-houses.component.css',
 })
-export class AddHousesComponent {}
+export class AddHousesComponent {
+  displayForm() {}
+  // constructor(
+  //   public name: string,
+  //   public city: string,
+  //   public state: string,
+  //   public availableUnits: number,
+  //   public wifi: boolean,
+  //   public laundry: boolean
+  // ) {}
+}
--- src/app/display-form/display-form.component.ts
+++ src/app/display-form/display-form.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { AddHousesComponent } from '../add-houses/add-houses.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-display-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, AddHousesComponent],
   templateUrl: './display-form.component.html',
-  styleUrl: './display-form.component.css'
+  styleUrl: './display-form.component.css',
 })
 export class DisplayFormComponent {
+  submitted = false;
 
+  onSubmit() {
+    this.submitted = true;
+  }
 }
