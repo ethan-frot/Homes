e34e2877247a4a54fea17326353dd38c376ff53d
CassandraDM <146823946+CassandraDM@users.noreply.github.com>
2024-01-12 13:48:32 +0100

    correct toggle like

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -4,43 +4,42 @@ import { Component } from '@angular/core';
   selector: 'app-favorites',
   standalone: true,
   imports: [],
-  template: ` <svg
-    (click)="heartFill()"
-    class="heart"
-    width="64px"
-    height="64px"
-    viewBox="0 0 24 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <g id="SVGRepo_bgCarrier" stroke-width="0" />
+  template: `
+    <svg
+      (click)="heartFill()"
+      [class.full]="isFull"
+      class="heart"
+      width="64px"
+      height="64px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0" />
 
-    <g
-      id="SVGRepo_tracerCarrier"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-
-    <g id="SVGRepo_iconCarrier">
-      <path
-        d="M15.7 4C18.87 4 21 6.98 21 9.76C21 15.39 12.16 20 12 20C11.84 20 3 15.39 3 9.76C3 6.98 5.13 4 8.3 4C10.12 4 11.31 4.91 12 5.71C12.69 4.91 13.88 4 15.7 4Z"
-        stroke="#605dc8"
-        stroke-width="2"
+      <g
+        id="SVGRepo_tracerCarrier"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
-    </g>
-  </svg>`,
+
+      <g id="SVGRepo_iconCarrier">
+        <path
+          d="M15.7 4C18.87 4 21 6.98 21 9.76C21 15.39 12.16 20 12 20C11.84 20 3 15.39 3 9.76C3 6.98 5.13 4 8.3 4C10.12 4 11.31 4.91 12 5.71C12.69 4.91 13.88 4 15.7 4Z"
+          stroke="#605dc8"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+    </svg>
+  `,
   styleUrl: './favorites.component.css',
 })
 export class FavoritesComponent {
-  heartFill() {
-    let heartList = document.querySelectorAll('.heart');
+  isFull: boolean = false;
 
-    heartList.forEach((heart) => {
-      heart.addEventListener('click', (e) => {
-        heart.classList.toggle('full');
-      });
-    });
+  heartFill() {
+    this.isFull = !this.isFull;
   }
 }
