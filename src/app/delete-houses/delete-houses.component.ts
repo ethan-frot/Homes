e4915276190d7183b13bb92a5b0305ddbdf095e7
Daria Karpenko <dariakarpenko@MacBook-Air-de-Daria.local>
2024-01-12 11:37:36 +0100

    added functionality for deleting an item

--- src/app/delete-houses/delete-houses.component.ts
+++ src/app/delete-houses/delete-houses.component.ts
@@ -1,10 +1,11 @@
 import { Component, ElementRef, Input, Renderer2 } from '@angular/core';
 import { HousingLocation } from '../housinglocation';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-delete-houses',
   standalone: true,
-  imports: [],
+  imports: [HomeComponent],
   templateUrl: './delete-houses.component.html',
   styleUrls: ['./delete-houses.component.css'],
   template: `
@@ -16,7 +17,15 @@ import { HousingLocation } from '../housinglocation';
 export class DeleteHousesComponent {
   @Input() housingLocation!: HousingLocation;
 
+  constructor(private el: ElementRef) {}
+
   confirmDeletion(): void {
-    var result = confirm('Do you want to delete the result?');
+    if (confirm('Do you want to delete this housing location?')) {
+      const nativeElement = this.el.nativeElement;
+      const parentElement = nativeElement.closest('.listing');
+      if (parentElement) {
+        parentElement.style.display = 'none';
+      }
+    }
   }
 }
