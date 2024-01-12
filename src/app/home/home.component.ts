4e711c9ff18033dede2e4f0e7813869fe44e54ff
1u1ucas <lucasleveque17@gmail.com>
2024-01-12 15:05:20 +0100

    color all set

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housinglocation';
 import { HousingService } from '../housing.service';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HousingLocationComponent],
+  imports: [CommonModule, HousingLocationComponent, AppComponent],
   template: `
     <section class="bar">
       <form>
@@ -65,12 +66,62 @@ export class HomeComponent {
 
   private updateCSS(type: string, value: string): void {
     const header = document.querySelector('header') as HTMLElement;
+    const button = document.querySelector('button') as HTMLElement;
+    const brandName = document.querySelector('.brand-name') as HTMLElement;
+    const sections = document.querySelectorAll(
+      'section'
+    ) as NodeListOf<HTMLElement>;
+    const textInput = document.querySelector(
+      'input[type="text"]'
+    ) as HTMLElement;
+    const listings = document.querySelectorAll(
+      '.listing'
+    ) as NodeListOf<HTMLElement>;
+    const listingHeadings = document.querySelectorAll(
+      '.listing-heading'
+    ) as NodeListOf<HTMLElement>;
+    const listingA = document.querySelectorAll(
+      'section.listing a'
+    ) as NodeListOf<HTMLElement>;
+    const listingFeatures = document.querySelectorAll(
+      '.listing-features > .section-heading'
+    ) as NodeListOf<HTMLElement>;
+    const labels = document.querySelectorAll(
+      'label'
+    ) as NodeListOf<HTMLElement>;
+    const btns = document.querySelectorAll(
+      'button.primary'
+    ) as NodeListOf<HTMLElement>;
 
     switch (type) {
       case 'color':
-        if (header) {
-          header.style.boxShadow = `0px 5px 25px var(--${value}-shadow-color)`;
-        }
+        brandName.style.boxShadow = `0px 5px 25px var(--${value}-shadow-color)`;
+        button.style.border = `solid 1px var(--${value}-primary-color)`;
+        button.style.background = `var(--${value}-primary-color)`;
+        textInput.style.border = `solid 1px var(--;${value}-primary-color)`;
+        brandName.style.background = `var(--${value}-background-color)`;
+        sections.forEach((section) => {
+          section.style.background = `var(--${value}-background-color)`;
+        });
+        listingHeadings.forEach((heading) => {
+          heading.style.color = `var(--${value}-primary-color)`;
+        });
+        listings.forEach((listing) => {
+          listing.style.background = `var(--${value}-accent-color)`;
+        });
+        listingA.forEach((a) => {
+          a.style.color = `var(--${value}-primary-color)`;
+        });
+        listingFeatures.forEach((feature) => {
+          feature.style.color = `var(--${value}-secondary-color)`;
+        });
+        labels.forEach((label) => {
+          label.style.color = `var(--${value}-secondary-color)`;
+        });
+        btns.forEach((btn) => {
+          btn.style.border = `var(--${value}-primary-color)`;
+          btn.style.background = `var(--${value}-primary-color)`;
+        });
         break;
     }
   }
